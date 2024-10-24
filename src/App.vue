e5e57ffee6c9a7b8a16cff9e01ed42aa24651135
noelardau <noelardau@gmail.com>
2024-10-24 08:41:42 +0400

    le debut

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  
+  <div class="w-full h-16 bg-yellow-400 p-3">
+      <div class="font-bold text-2xl">
+        AMBININTSOA R. Noëlardau 
+      </div>
+  </div>
+
+
+
+</template>
+
+<script setup>
+
+
+
+
+</script>
