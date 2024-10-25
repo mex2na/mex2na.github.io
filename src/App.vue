db5e7398838fa8aa1d5e273fd9b02cc5e30f3794
noelardau <noelardau@gmail.com>
2024-10-25 15:54:12 +0400

    Partie profil terminée

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
+<script setup>
+   import Profile from './components/Profile.vue';     
+
+
+</script>
+
 <template>
   
-  <div class="w-full h-16 bg-yellow-400 p-3">
-      <div class="font-bold text-2xl">
-        AMBININTSOA R. Noëlardau 
-      </div>
-  </div>
+    <div id="aside" class="w-2/5 bg-[#F8C822] md:h-48 md:w-full py-4">
+            <Profile/>
 
+    </div>
+
+    
+    <div id="content" class="w-3/5 md:grow md:w-full">
+
+
+    </div>
 
 
 </template>
 
-<script setup>
 
 
 
-
-</script>
---
+++ src/components/Profile.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import photo from "../assets/images/photo_CV.jpg" 
+
+    
+
+
+</script>
+<template>
+
+<div class="flex-col gap-5  text-black p-2">
+
+    <div class="flex justify-center pb-2">
+        <img :src="photo" alt="pdp" class="w-40 object-fill h-40 rounded-full">
+    </div>
+    
+    <div>
+        <p class="text-2xl font-medium text-center">AMBININTSOA </p>
+        <p class="text-base text-center">Rodolphino Noëlardau</p>
+  </div>
+
+    <div class="flex  px-10 mt-5">
+        <span class="text-sm text-left  pb-5 border-black border-b-2 px-6 ">Full-stack developer, passionate about Javascript but open to other technologies</span>
+    </div>
+
+</div>
+
+    
+</template>
